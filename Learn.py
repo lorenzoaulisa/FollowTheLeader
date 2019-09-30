7de40857e51cb1252f165ea3757105fcba5b8fe7
lorenzoaulisa <lorenzoaulisa05@gmail.com>
2019-09-30 15:50:32 -0500

    First commit

--- Learn.py
+++ Learn.py
@@ -1,6 +1,6 @@
-import turtle
-
 print("hello world")
 print("howdy y'all")
-
-turtle.exitonclick()
+print("Roosters: ", 100 - 25 * 3 % 4)
+print("I shall now count my dead bodies in my basement")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print()
