130878213da9537c33f0a228acebd6073f11f50d
lorenzoaulisa <lorenzoaulisa05@gmail.com>
2019-10-01 15:46:36 -0500

    First commit

--- Learn.py
+++ Learn.py
@@ -1,7 +1,20 @@
+#The program starts and a turtle is created
+import turtle
+#The program will say the text "hello world" in the console.
 print("hello world")
+#The program will say the text "howdy y'all" in the console.
 print("howdy y'all")
-print("Roosters: ", 100 - 25 * 3 % 4)
+#The program will say the text "I will now count my roosters" in the console.
+print(" I will now count my roosters")
+#The program will now calculate the number of roosters Lorenzo has on his farm
+print("Roosters: ", 100.0 +21.5 +17.5 - 25 * 3 % 4)
+#After calculating that Lorenzo has 136 roosters on his farm the program will now say
+#"I shall now count the number of dead bodies in Lorenzo's basement
 print("I shall now count my dead bodies in my basement")
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
-print()
-# huhbyuhy
+#The program will now calculate how many dead bodies Lorenzo has in his basement
+print(3.55 + 2.2 + 1.5 - 5 + 4 % 2 - 1 / 4 + 6)
+#After calculating that Lorenzo has 8 dead bodies in Lorenzo's basement, the program shall now
+# have the turtle leave the area
+turtle.forward(500)
+#The program ends
+turtle.exitonclick()
